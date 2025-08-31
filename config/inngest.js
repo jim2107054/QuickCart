1e2c017f2c4e12aef802df737723458f594f8e24
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-31 06:58:11 +0600

    create inngest function to save user data to database

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,29 @@
 import { Inngest } from "inngest";
+import connectDB from "./db.js";
+import User from "@/models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 
 // Inngest Function to save user data to a database
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  {
+    event: "clerk/user.created",
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectDB();
+    // Save userData to the database
+    await User.create(userData);
+  }
+);
